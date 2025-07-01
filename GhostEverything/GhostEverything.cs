b8c7c986de3903fd21fceed0d1ea63372beb226a
coderCleric <56094451+coderCleric@users.noreply.github.com>
2025-07-01 15:37:05 -0600

    Actually push the mod

--- GhostEverything/GhostEverything.cs
+++ GhostEverything/GhostEverything.cs
@@ -1,29 +1,67 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
+using System.CodeDom;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GhostEverything
 {
     public class GhostEverything : ModBehaviour
     {
-        private void Awake()
-        {
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
+        private int toggleMask = (1 << 0) | (1 << 9) | (1 << 15) | (1 << 28) | (1 << 2);
+        private int hiddenMask = 0;
+        private int visibleMask = 0;
+        private bool hidden = false;
+        private bool validScene = false;
 
         private void Start()
         {
             // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(GhostEverything)} is loaded!", MessageType.Success);
+            ModHelper.Console.WriteLine($"{nameof(GhostEverything)} is loaded!", MessageType.Success);
 
-
-            // Example of accessing game code.
+            //Only let it be used in specific scenes
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
-                if (loadScene != OWScene.SolarSystem) return;
-                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+                validScene = loadScene == OWScene.SolarSystem || loadScene == OWScene.EyeOfTheUniverse;
             };
+
+            //When the player wakes up, reset the mod
+            GlobalMessenger.AddListener("WakeUp", OnceAwake);
         }
+
+        /**
+         * Wait a frame, then reset the mod state
+         */
+        private void OnceAwake()
+        {
+            ModHelper.Events.Unity.FireOnNextUpdate(() =>
+            {
+                visibleMask = Locator.GetPlayerCameraController()._origCullingMask;
+                hiddenMask = visibleMask ^ toggleMask;
+                hidden = true; 
+                Locator.GetPlayerCamera().cullingMask = hiddenMask;
+                Locator.GetPlayerCameraController()._origCullingMask = hiddenMask;
+            });
+        }
+
+        private void Update()
+        {
+            if (Keyboard.current[Key.K].wasPressedThisFrame && validScene && Locator.GetPlayerCamera() != null)
+            {
+                if(hidden)
+                {
+                    hidden = false;
+                    Locator.GetPlayerCamera().cullingMask = visibleMask;
+                    Locator.GetPlayerCameraController()._origCullingMask = visibleMask;
+                }
+                else
+                {
+                    hidden = true;
+                    Locator.GetPlayerCamera().cullingMask = hiddenMask;
+                    Locator.GetPlayerCameraController()._origCullingMask = hiddenMask;
+                }
+            }
+        }
+
     }
 }
